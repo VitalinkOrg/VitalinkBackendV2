4eb315931694ec0d1798f19b714314cb63f8c1d3
fabian7593 <fabianr7593@gmail.com>
2025-06-02 18:47:15 -0600

    added logic of notifications by user id

--- src/modules/01_General/notification/routers/UserNotificationRoutes.ts
+++ src/modules/01_General/notification/routers/UserNotificationRoutes.ts
@@ -1,9 +1,19 @@
 import { Request, Response, GenericRoutes,
-         RequestHandler, RequestHandlerBuilder} from "@modules/index";
+         RequestHandler, RequestHandlerBuilder,
+         FindManyOptions} from "@modules/index";
 import { default as UserNotificationController } from '@modules/01_General/notification/controllers/UserNotificationController';
 import { UserNotificationDTO, requiredBodyListUserNotifications } from '@modules/01_General/notification/index';
 
 class UserNotificationRoutes extends GenericRoutes {
+
+    private buildBaseFilters(): FindManyOptions {
+      return {
+        relations: ["user_receive", "user_send", "notification"],
+        where: {}
+      };
+    }
+
+    
     constructor() {
         super(new UserNotificationController(), "/usernotification");
     }
@@ -11,12 +21,19 @@ class UserNotificationRoutes extends GenericRoutes {
     protected initializeRoutes() {
         this.router.get(`${this.getRouterName()}/get`, async (req: Request, res: Response) => {
   
+            const filters = this.buildBaseFilters();
             const requestHandler : RequestHandler = 
                                     new RequestHandlerBuilder(res,req)
                                     .setAdapter(new UserNotificationDTO(req))
                                     .setMethod("getUserNotificationById")
                                     .isValidateRole("USER_NOTIFICATION")
                                     .isLogicalDelete()
+                                    .setFilters(filters)
+                                    .setDynamicRoleValidationByEntityField([
+                                        ["CUSTOMER", "user_receive.id"],
+                                        ["LEGAL_REPRESENTATIVE", "user_receive.id"],
+                                        ["FINANCE_ENTITY", "user_receive.id"]
+                                      ])
                                     .build();
         
             this.getController().getById(requestHandler);
@@ -25,12 +42,19 @@ class UserNotificationRoutes extends GenericRoutes {
 
          this.router.get(`${this.getRouterName()}/get_all`, async (req: Request, res: Response) => {
         
+            const filters = this.buildBaseFilters();
             const requestHandler : RequestHandler = 
                                     new RequestHandlerBuilder(res,req)
                                     .setAdapter(new UserNotificationDTO(req))
                                     .setMethod("getUserNotification")
                                     .isValidateRole("USER_NOTIFICATION")
+                                    .setFilters(filters)
                                     .isLogicalDelete()
+                                    .setDynamicRoleValidationByEntityField([
+                                        ["CUSTOMER", "user_receive.id"],
+                                        ["LEGAL_REPRESENTATIVE", "user_receive.id"],
+                                        ["FINANCE_ENTITY", "user_receive.id"]
+                                      ])
                                     .build();
         
           //TODO JUST WORK AFTER RUN STORED PROCEDURE src\data\db_scripting\01_db_stored_procedures.sql
