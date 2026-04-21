181e04b6115118cc1a4027378291e53e556c0bf3
fabian7593 <fabianr7593@gmail.com>
2026-04-21 13:16:59 -0600

    fix deploy sh

--- deploy_tenshi.dev.sh
+++ deploy_tenshi.dev.sh
@@ -4,10 +4,10 @@ set -e
 
 echo "🌿 Deploy VITALINK Backend"
 
-cd /home/ubuntu/Vitalink-Backend
+cd /home/vita-backend-dev/VitalinkBackendV2
 
 echo "🔄 Pull latest"
-git pull origin main
+git pull origin dev
 
 echo "📦 Install deps"
 npm ci --omit=dev
@@ -16,7 +16,7 @@ echo "🏗 Build"
 npm run build
 
 echo "♻️ Reload PM2"
-pm2 reload ecosystem.config.js || pm2 start ecosystem.config.js
+pm2 reload ecosystem.config.dev.js || pm2 start ecosystem.config.dev.js
 
 echo "💾 Save"
 pm2 save
